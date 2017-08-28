9303aec1fc4dd96e774ac530f0891d8450bb40c7
Matthew Kloecker <matthew.kloecker@gmail.com>
2017-08-28 15:57:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const app = "I don't do much."
 //start
 var kittens=["Milo", "Otis", "Garfield"];
+
+//Destructively Append Kitten 'Ralph'
+function.destructivelyAppendKitten(array, name){
+  array.push(element)
+}
